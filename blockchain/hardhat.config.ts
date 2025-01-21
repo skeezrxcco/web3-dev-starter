f7b590e87b1d2d885d18bcf5d0f6bed5f432b928
Ricky <silva.pires.ricardo86@outlook.com>
2025-01-21 22:53:24 +0100

    Add dotenv support for mainnet forking and remove unused env example files. Update package dependencies accordingly

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,11 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const { MAINNET_RPC_URL, MAINNET_BLOCK_NUMBER } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     hardhat: {
-      chainId: 31337
+      chainId: 31337,
+      forking: {
+        url: MAINNET_RPC_URL || '',
+        blockNumber: MAINNET_BLOCK_NUMBER ? parseInt(MAINNET_BLOCK_NUMBER) : undefined
+      }
     },
   },
   paths: {
